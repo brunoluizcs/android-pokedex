500d700ea159cd8a09014dd04c7d3276c5ebff07
n3w733n <niltinhosupriano@gmail.com>
2020-10-27 18:06:54 -0300

    Criação da tela sobre, personalizando status bar.

---
+++ app/src/main/java/com/becarios/pokedex/presentation/about/AboutActivity.kt
@@ -0,0 +1,12 @@
+package com.becarios.pokedex.presentation.about
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.becarios.pokedex.R
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+}
--- app/src/main/java/com/becarios/pokedex/presentation/pokemons/PokemonsActivity.kt
+++ app/src/main/java/com/becarios/pokedex/presentation/pokemons/PokemonsActivity.kt
@@ -1,12 +1,27 @@
 package com.becarios.pokedex.presentation.pokemons
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import com.becarios.pokedex.R
 
 class PokemonsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pokemons)
+        setContentView(R.layout.activity_about)
+        backGroundColor()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    fun backGroundColor() {
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.statusBarColor = ContextCompat.getColor(this, android.R.color.transparent)
+        window.navigationBarColor = ContextCompat.getColor(this, android.R.color.transparent)
+        window.setBackgroundDrawableResource(R.drawable.splash_gradient_light)
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
     }
 }
