b9ebfedc713c5202a0aeafb372a04a5b25c66e07
Carlosjr01 <carlos.udi@live.com>
2020-11-04 00:14:57 -0300

    dimens.xml atualizado, foi alterado o voltar de imageView para button para sanar o problema de acessibilidade tanto na activity_about.xml quanto na activity_pokemons.xml e excluido pasta home por ser redundante.

--- app/src/main/java/com/becarios/pokedex/presentation/intro/SplashActivity.kt
+++ app/src/main/java/com/becarios/pokedex/presentation/intro/SplashActivity.kt
@@ -1,7 +1,6 @@
 package com.becarios.pokedex.presentation.intro
 
 import android.content.Intent
-import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
@@ -9,40 +8,32 @@ import android.view.View
 import android.view.WindowManager
 import com.becarios.pokedex.R
 import com.becarios.pokedex.presentation.pokemons.PokemonsActivity
-import java.util.*
 
 @Suppress("DEPRECATION")
 class SplashActivity : AppCompatActivity() {
-    private var timer: Timer? = null
-    private lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
         hideStatusBarAndFullScreen()
-
-        timer = Timer()
-        handler = Handler()
-
-        handler.postDelayed(object : Runnable {
-            override fun run() {
-                startActivity(
-                    Intent(
-                        this@SplashActivity, PokemonsActivity::class.java
-                    )
-                )
-                finish()
-            }
-        }, 3000)
+        delayStartActivity()
     }
-    //Thread.sleep(3000)
 
     private fun hideStatusBarAndFullScreen() {
         window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+            WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
 
         window.decorView.apply {
-            systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
+            systemUiVisibility =
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_FULLSCREEN
         }
+    }
+
+    private fun delayStartActivity() {
+        Handler().postDelayed({
+            startActivity(Intent(this@SplashActivity, PokemonsActivity::class.java))
+            finish()
+        }, 3000)
     }
 }
